84a2b26c78cb9df94d8e22ad5c304b07329e0e8c
thanhtung5598 <thanhtung5598@gmail.com>
2020-12-07 21:05:44 +0700

    Update

--- app/src/main/java/com/example/music_lethanhtung/MainActivity.java
+++ app/src/main/java/com/example/music_lethanhtung/MainActivity.java
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
         listMusic.add(new Music("Wildest Dreams","Taylor Swift", R.raw.wildest_dreams, R.drawable.wildest_dreams,5));
         listMusic.add(new Music("Anne-Marie","Anne-Marie -2002", R.raw.remember, R.drawable.remember,5));
         listMusic.add(new Music("I Need Your Love","Madilyn Bailey", R.raw.i_need_your_love, R.drawable.i_need_your_lovei_need_your_love,4));
-        listMusic.add(new Music("Vì Em Nhớ Anh","Mina Young Cover", R.raw.vi_em_nho_anh, R.drawable.vi_em_nho_anh,4));
-        listMusic.add(new Music("I'm gonna lose you","Jasmine Thompson", R.raw.like_i_m_gonna_lose_you, R.drawable.gonna_lose,3));
 
         recyclerView = findViewById(R.id.rcv);
 
